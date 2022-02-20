3d7857f52713bb3c8fd725c7cefa5396cd510b6c
Harendra Prajapati <harendraprajapati72@gmail.com>
2022-02-21 02:49:17 +0530

    Add files via upload

---
+++ Varrables.java
@@ -0,0 +1,8 @@
+class Varrables {
+    public static void main(String[] args) {
+        int totalStudents=100;
+        int studentInClass=60;
+        System.out.println("Absent stiudents : "+(totalStudents-studentInClass));
+
+    }
+}
